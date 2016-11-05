4c84a9efe14dd50164ef7d4839095fcb12673615
leodrivera <leodrivera@gmail.com>
2016-11-05 16:32:49 -0200

    Início

---
+++ Cliente.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+import socket, os, time
+
+HOST = '127.0.0.1'    # The remote host
+PORT = 50008              # The same port as used by the server
+soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPv4,tipo de socket
+soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #forçar que o socket desaloque a porta quando fechar o código
+
+while 1:
+	try:
+		soc.connect((HOST, PORT))  #Abre uma conexão com IP e porta especificados
+		break
+	except:
+		time.sleep(1)
+
+def teste(valor,l): # função para tratamento de erro de entrada de valores
+	while 1:		
+		try:	
+			if len(valor) <= l: #Se o valor digitado for vazio, ele executa uma exceção
+				raise	
+			break	#Se o valor for válido, ele quebra o loop
+		except Exception, err:
+			valor=raw_input('Valor inválido, digite novamente\n')	#Exceção pelo valor vazio
+	return valor #retorna valor válido
+
+c=raw_input('Digite 0 para cadatrar novo usuário ou 1 para logar em usuário já existente\n')
+
+while (c != '1') and (c != '0'):
+	c=raw_input('Digite 0 para cadatrar novo usuário ou 1 para logar em usuário já existente\n')
+
+if c== '0': # Cadastro de novo usuário
+	soc.sendall('Adiciona_usuario')
+
+	while 1:            #Laço do nome
+		nome=raw_input('Digite o nome do novo usuário\n')
+		nome=teste(nome,0) # Teste de entrada vazio
+		soc.sendall(nome) # Envia nome para o servidor
+		check = soc.recv(1024) #Recebe resposta se o nome é repetido ou não
+		if check == 'ok':
+			print "Tá tranquilo, tá favorável"
+			break # Se passar no teste, ele sai
+		print ('Nome existente.\n')
+	
+	#cadastro senha
+	senha=raw_input('Digite a nova senha')
+	senha=teste(senha,0) # Teste de entrada vazio
+	soc.sendall(senha) # Envia senha para o servidor	
+
+
+        #cadastro telefone
+	telefone=raw_input('Digite o telefone do novo usuário.\nEx: xxxx-xxxx\n')
+	telefone=teste(telefone,7) # Teste de entrada vazio
+	soc.sendall(telefone) # Envia telefone para o servidor
+
+	
+	#cadastro endereço
+	end=raw_input('Digite o endereço do novo usuário')
+	end=teste(end,0) # Teste de entrada vazio
+	soc.sendall(end) # Envia endereço para o servidor
+	
+
+
+
+	
+
+
+
+
+
+
+
+
---
+++ Servidor.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# encoding: utf-8
+ 
+from threading import Thread
+import socket, os
+
+HOST = ''                 # Symbolic name meaning all available interfaces
+PORT = 50008              # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPv4,tipo de socket (TCP)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #forçar que o socket desaloque a porta quando fechar o código
+s.bind((HOST, PORT)) #liga o socket com IP e porta
+s.listen(1) #espera chegar pacotes na porta especificada
+conn, addr = s.accept()#Aceita uma conexão
+
+print 'Connectado ao', addr
+print "Servidor funcionado com o PID", os.getpid()
+print "Esperando pelos clientes\n"
+
+
+
+a = conn.recv(1024) #Cliente querendo cadastrar
+
+print a
+
+b = conn.recv(1024) #Recebe nome do servidor
+
+print b
+
+c = conn.sendall('ok') #Não é repetido
+
+
+b = conn.recv(1024) #Recebe senha do servidor
+
+print b
+
+b = conn.recv(1024) #Recebe telefone do servidor
+
+print b
+
+b = conn.recv(1024) #Recebe endereço do servidor
+
+print b
+
+
+"""
+class usuario:
+
+	def __init__(self, nome, telefone, endereco, email, senha):
+
+"""
+
+
+
+
+
+
+
+
+
+
