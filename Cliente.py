3400e0c6c4ff023d5d2885efa7f5e2cf7f55c433
filipe <filipesilveira@id.uff.br>
2016-11-11 17:15:29 -0200

    edicao cliente

--- Cliente.py
+++ Cliente.py
@@ -30,13 +30,13 @@ loged=0
 c=raw_input('Digite 0 para cadatrar novo usuário ou 1 para logar em usuário já existente\n')
 
 while (c != '1') and (c != '0'):
-	c=raw_input('Digite 0 para cadatrar novo usuário ou 1 para logar em usuário já existente\n')
+	c=raw_input('Digite 0 para cadatrar novo usuário ou 1 para logar em usuário já existente:\n')
 
 if c== '0': # Cadastro de novo usuário
 	# soc.sendall('Adiciona_usuario')
 
 	while 1:  #Laço do cadastro
-		nome=raw_input('Digite o nome do novo usuário\n')
+		nome=raw_input('Digite o nome do novo usuário:\n')
 		nome=teste(nome,0) # Teste de entrada vazio
 		#soc.sendall(nome) # Envia nome para o servidor
 		#check = soc.recv(1024) #Recebe resposta se o nome é repetido ou não
@@ -44,42 +44,41 @@ if c== '0': # Cadastro de novo usuário
 		#	print "Tá tranquilo, tá favorável"
 		#	break # Se passar no teste, ele sai
 		#print ('Nome existente.\n')
-		telefone=raw_input('Digite o telefone do novo usuário.\nEx: xxxx-xxxx\n')
+		telefone=raw_input('Digite o telefone do novo usuário.\nEx: xxxx-xxxx:\n')
 		telefone=teste(telefone,7) # Teste de entrada vazio
 		#soc.sendall(telefone) # Envia telefone para o servidor
-		ender=raw_input('Digite o endereço do novo usuário\n')
+		ender=raw_input('Digite o endereço do novo usuário:\n')
 		ender=teste(ender,0) # Teste de entrada vazio
 		#soc.sendall(end) # Envia endereço para o servidor
-		email=raw_input('Digite o email do novo usuário\n')
+		email=raw_input('Digite o email do novo usuário:\n')
 		email=teste(email,40)
 		#
-		senha = raw_input('Digite a nova senha de no mínimo 4 dígitos\n')
+		senha = raw_input('Digite a nova senha de no mínimo 4 dígitos:\n')
 		senha = teste(senha, 3)  # Teste de entrada vazio
 		# soc.sendall(senha) # Envia senha para o servidor
-		soc.sendall('Adiciona_usuario'+","+nome+","+ender+","+email+","+senha)
+		soc.sendall('Adiciona_usuario'+","+nome+","+telefone+","+ender+","+email+","+senha)
 		re=soc.recv(1024)
 		re=re.split(',')
 		if re(0)=="ok":
-			print('Usuário cadastrado com sucesso')
+			print('Usuário cadastrado com sucesso, usuário logado.\n')
 			loged=1
 			break
 		else:
-
-			nome=raw_input('Nome de usuário já utilizado, favor digitar um novo')
-
-
-
-
-		break
-
-0
-
-
-	
-
-
-
-
+			print('Nome de usuário já utilizado\n')
+else:
+	while (1):
+		nome = raw_input('Digite o nome do usuário:\n')
+		nome = teste(nome, 0)
+		senha = raw_input('Digite sua senha:\n')
+		senha = teste(senha, 3)
+		soc.sendall("Faz_login,"+nome+','+senha)
+		rec = soc.recv(1024)
+		if rec(0) == 0:
+			print('Usuário logado com sucesso')
+			loged=1
+			break
+		else:
+			print('Usuário não cadatrado\n')
 
 
 
