6e08388245380dd12fbcaf7fe0e308aef7d99dcb
filipe <filipesilveira@id.uff.br>
2016-11-11 19:46:30 -0200

    edicao final dia 11

--- Servidor.py
+++ Servidor.py
@@ -7,7 +7,7 @@ import socket, os
 class usuario:
     nome = None
     telefone = 0
-    endereço = None
+    endereco = None
     email = None
     senha = None
 
@@ -18,33 +18,52 @@ class usuario:
         self.email = email
         self.senha = senha
 
-    def adiciona_usuario(self):
-        for i in range(len(texto)):
-            if self.nome + "\n" == texto[i]:
-                conn.sendall('not_ok')
-            else:
-                f = open('clientes')
 
 
+
+
+class ctr_usuarios:
+    lista_usuarios=[]
+    def __init__(self):
+        print'inicia lista de usuários'
+    def recebe_usuario(self,usuarios):
+        self.lista_usuarios.append(usuario)
+
+    def checa_nomes(self,nome):
+        resp='tem'
+        for i in self.lista_usuarios:
+            if nome==i:
+                resp='n_tem'
+        return resp
 
 
 def servidor(conn):
-	print 'Conectado por', addr, "\n"
-
-	while 1:
+    print 'Conectado por', addr, "\n"
+    while 1:
         a = conn.recv(1024)  # Cliente querendo cadastrar com os dados contidos em a
-   		if not a: break #Sai do loop caso valor seja nulo ou 0
-        user = a.split(",") #Informações do usuário separados na lista user
-        if a[0] == 'Adiciona_usuario': # adiciona_usuario
-            for i in range(len(texto)):
-                if a[1]+ "\n" == texto[i]:
-                    conn.sendall('not_ok')
-                else:
-                    a[0] = usuario(user)
-            texto.acquire()
+        user = a.split(",")  # Informações do usuário separados na lista user
+        print user
 
-            texto.release()
+"""""
+
+        if user[0] == 'Adiciona_usuario': # adiciona_usuario
+            for i in range(nclientes):
+
+
+
+                conn.sendall('not_ok'+'cl_usado')
+
             else:
+"""""
+            #texto.acquire()
+
+            #texto.release()
+
+
+
+
+
+
             #login do usuario
 
 
@@ -57,44 +76,40 @@ if __name__ == '__main__':  ###Programa principal
 
     except IOError: pass
 
-    HOST = ''                 # Symbolic name meaning all available interfaces
+    HOST = '127.0.0.1'                 # Symbolic name meaning all available interfaces
     PORT = 50008              # Arbitrary non-privileged port
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #IPv4,tipo de socket (TCP)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #forçar que o socket desaloque a porta quando fechar o código
     s.bind((HOST, PORT)) #liga o socket com IP e porta
-    s.listen(1) #espera chegar pacotes na porta especificada
-    conn, addr = s.accept()#Aceita uma conexão
+    #s.listen(1) #espera chegar pacotes na porta especificada
+    #conn, addr = s.accept()#Aceita uma conexão
 
     print "Servidor funcionado"
     print "Esperando pelos clientes\n"
 
-    clientes=0
+    nclientes=0
+
+    clientes={}
 
     while 1:
+        print 'escutando'
         s.listen(1)
         conn, addr = s.accept()  # Aceita uma conexão
-        clientes += 1
-        PID.append(conn.recv(1024))  # Recebe o PID do cliente
-        print "Cliente conectado com o PID", PID[clientes - 1]
-        print "Número de clientes conectados", clientes
+        nclientes += 1
+        #PID.append(conn.recv(1024))  # Recebe o PID do cliente
+        #print "Cliente conectado com o PID", PID[clientes - 1]
+        print "Número de clientes conectados", nclientes
         t = Thread(target=servidor, args=(conn,))
         t.start()
 
 
 
 
-    print a
+    #print a
 
-    b = conn.recv(1024) #Recebe nome do servidor
+    #b = conn.recv(1024) #Recebe nome do servidor
 
 
-
-    """
-    class usuario:
-
-        def __init__(self, nome, telefone, endereco, email, senha):
-
-    """
 
 
 
